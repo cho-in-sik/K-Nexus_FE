aebba6097ae5810f8b2fdc4ae9676dddb4fd0926
조인식 <imsif42@naver.com>
2024-08-27 19:56:29 +0900

    feat: 온보딩 저장 성공

--- src/actions/onBoarding.ts
+++ src/actions/onBoarding.ts
@@ -1,22 +1,33 @@
 'use server';
 
 import { createServerSupabaseClient } from '@/app/utils/supabase/server';
+import { redirect } from 'next/navigation';
 
 function handleError(error: any) {
+  console.error('Supabase Error:', error.message);
   throw new Error(error);
 }
 
-export async function createOnboradingInfo({ steps, id }: any) {
+export async function createOnboradingInfo({
+  steps,
+  id,
+}: {
+  steps: string[];
+  id: string;
+}) {
   const supabase = await createServerSupabaseClient();
   console.log(steps);
 
-  const { data, error } = await supabase
+  const { data, error, status } = await supabase
     .from('users')
-    .update({ onBoardingInfo: steps, onBoarding: true })
+    .update({ onboarding_info: steps, onboarding: true })
     .eq('id', id);
 
   if (error) {
     handleError(error);
+  }
+  if (status === 204) {
+    redirect('/home');
   }
 
   return data;
--- src/app/(beforeLogin)/onBoarding/page.tsx
+++ src/app/(beforeLogin)/onBoarding/page.tsx
@@ -1,11 +1,24 @@
 import { createServerSupabaseClient } from '@/app/utils/supabase/server';
 import OnBoardingClientComponent from './_components/OnBoardingClientCompoent';
+import { redirect } from 'next/navigation';
 
 export default async function Page() {
   const supabase = await createServerSupabaseClient();
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
 
-  return <OnBoardingClientComponent id={session?.user.id} />;
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  const uid = user?.id;
+
+  const { data: onboarding } = await supabase
+    .from('users')
+    .select('onboarding')
+    .eq('id', uid)
+    .single();
+
+  if (onboarding?.onboarding) {
+    redirect('/home');
+  }
+
+  return <OnBoardingClientComponent id={uid} />;
 }
