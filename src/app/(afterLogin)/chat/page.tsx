f6d68ac754fb2fd5af343cc399cc554083501c1a
조인식 <imsif42@naver.com>
2024-09-07 22:29:39 +0900

    feat: as

--- src/app/(afterLogin)/chat/page.tsx
+++ src/app/(afterLogin)/chat/page.tsx
@@ -17,12 +17,25 @@ export default async function Page() {
         <ChatListBox session={session} />
       </div>
 
-      <div className="absolute bottom-28 right-10">
-        <Link href={'/'}>
-          <button className="btn btn-circle bg-[#4376FE] btn-lg">
-            <Image src={plus} alt="plus" />
-          </button>
-        </Link>
+      <div className="absolute bottom-28 right-10 dropdown dropdown-top dropdown-end">
+        <button
+          tabIndex={0}
+          role="button"
+          className="btn btn-circle bg-[#4376FE] btn-lg"
+        >
+          <Image src={plus} alt="plus" />
+        </button>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+        >
+          <li>
+            <a>음성으로 만들기</a>
+          </li>
+          <li>
+            <a>채팅으로 만들기</a>
+          </li>
+        </ul>
       </div>
     </div>
   );
