5390a580c055aafd2be8f456b49ac028d93de831
조인식 <imsif42@naver.com>
2025-01-08 14:29:01 +0900

    fix: home 로그인 로직 수정

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,40 +1,59 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useQuery } from '@tanstack/react-query';
+
 import shopping from '@/../public/svgs/home/shopping.svg';
 import travel from '@/../public/svgs/home/travel.svg';
 import airport from '@/../public/svgs/home/airport.svg';
 import alphabet from '@/../public/svgs/home/alphabet.svg';
 import talk from '@/../public/svgs/home/talk.svg';
 import random from '@/../public/svgs/home/random.svg';
-import Image from 'next/image';
 
-import Link from 'next/link';
-import { useQuery } from '@tanstack/react-query';
-
-import { redirect } from 'next/navigation';
-
-import { userInfo } from '@/app/api/user';
 import { getLocalStorage } from '@/app/utils/handleToken';
+import { userInfo } from '@/app/api/user';
 
 export default function Page() {
-  const token = getLocalStorage();
+  const [token, setToken] = useState<string | null>(null);
+  const [isTokenChecked, setIsTokenChecked] = useState(false); // 초기화 여부 상태
+  const router = useRouter();
+
+  useEffect(() => {
+    const storedToken = getLocalStorage();
+    setToken(storedToken);
+    setIsTokenChecked(true); // 토큰 확인 완료
+  }, []);
+
+  useEffect(() => {
+    if (isTokenChecked && !token) {
+      setTimeout(() => {
+        router.push('/login');
+      }, 0);
+    }
+  }, [isTokenChecked, token, router]);
+
   const { data } = useQuery({
     queryKey: ['user'],
     queryFn: userInfo,
-    // enabled: !!token, // 토큰이 있을 때만 쿼리 실행
+    enabled: !!token,
   });
 
-  if (!token) {
-    redirect('/login');
+  if (!isTokenChecked) {
+    return null;
   }
 
-  console.log(data);
-  console.log(token);
+  if (!token) {
+    // 토큰이 없는 경우 로딩 상태를 보여줌
+    return <div>Redirecting to login...</div>;
+  }
 
   return (
     <div className="h-screen">
       <div className="w-full h-36 rounded-2xl border-2 border-[#D9D9D9] mt-20 mb-10 text-xl font-medium py-7 pl-6">
-        <h1>{`${data?.name} 님`}</h1>
+        <h1>{`${data?.name || ''} 님`}</h1>
         <h1>안녕하세요.</h1>
         <h1>오늘도 열심히 공부해봐요 💪🏼</h1>
       </div>
@@ -50,7 +69,7 @@ export default function Page() {
         <Link href={'/category/talk-with-friends'}>
           <div className="h-32 w-40 bg-[#E7E9EC] rounded-lg flex justify-center items-center">
             <div className="flex flex-col justify-center items-center gap-4">
-              <Image src={talk} alt="shopping" className="mt-4" />
+              <Image src={talk} alt="talk" className="mt-4" />
               <span className="text-sm font-medium mt-2">
                 talk with friends
               </span>
@@ -60,7 +79,7 @@ export default function Page() {
         <Link href={'/category/travel'}>
           <div className="h-32 w-40 bg-[#E7E9EC] rounded-lg flex justify-center items-center">
             <div className="flex flex-col justify-center items-center gap-4">
-              <Image src={travel} alt="shopping" className="mt-3" />
+              <Image src={travel} alt="travel" className="mt-3" />
               <span className="text-sm font-medium">travel</span>
             </div>
           </div>
@@ -68,7 +87,7 @@ export default function Page() {
         <Link href={'/category/learn-alphabet'}>
           <div className="h-32 w-40 bg-[#E7E9EC] rounded-lg flex justify-center items-center">
             <div className="flex flex-col justify-center items-center gap-4">
-              <Image src={alphabet} alt="shopping" className="mt-3" />
+              <Image src={alphabet} alt="alphabet" className="mt-3" />
               <span className="text-sm font-medium">alphabet</span>
             </div>
           </div>
@@ -76,7 +95,7 @@ export default function Page() {
         <Link href={'/category/airport'}>
           <div className="h-32 w-40 bg-[#E7E9EC] rounded-lg flex justify-center items-center">
             <div className="flex flex-col justify-center items-center gap-4">
-              <Image src={airport} alt="shopping" className="mt-3" />
+              <Image src={airport} alt="airport" className="mt-3" />
               <span className="text-sm font-medium">airport</span>
             </div>
           </div>
@@ -84,7 +103,7 @@ export default function Page() {
         <Link href={'/category/random-course'}>
           <div className="h-32 w-40 bg-[#E7E9EC] rounded-lg flex justify-center items-center">
             <div className="flex flex-col justify-center items-center gap-4">
-              <Image src={random} alt="shopping" className="mt-3" />
+              <Image src={random} alt="random" className="mt-3" />
               <span className="text-sm font-medium">random</span>
             </div>
           </div>
--- src/app/utils/handleToken.ts
+++ src/app/utils/handleToken.ts
@@ -1,6 +1,7 @@
-export const getLocalStorage = () => {
+export const getLocalStorage = (): string | null => {
   if (typeof window !== 'undefined') {
     const value: string | null = localStorage.getItem('token');
     return value ? value : null;
   }
+  return null;
 };
